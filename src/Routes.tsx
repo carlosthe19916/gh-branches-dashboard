09bf5a024b36648dfe3b6858e182c15618da960c
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-03-20 22:07:18 +0100

    restore /home page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { PageForbidden403 } from "./PresentationalComponents/Pages/PageForbidden403";
 import { PageHome } from "./PresentationalComponents/Pages/PageHome";
 import { PageBranches } from "./PresentationalComponents/Pages/PageBranches";
@@ -7,12 +7,13 @@ import { PageBranches } from "./PresentationalComponents/Pages/PageBranches";
 export const AppRoutes = () => {
   return (
     <Switch>
-      <Route path="/" component={PageHome} />
+      <Route path="/home" component={PageHome} />
       <Route
         path="/monitor/:owner/:repository/branches"
         component={PageBranches}
       />
       <Route path="/error403" component={PageForbidden403} />
+      <Route path="/" render={() => <Redirect to={"/home"} />} />
     </Switch>
   );
 };
